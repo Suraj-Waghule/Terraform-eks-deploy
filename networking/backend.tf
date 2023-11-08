f3877cc0effa241b89512dd0b7c59c52c108944c
Suraj-Waghule <134491679+Suraj-Waghule@users.noreply.github.com>
2023-11-08 23:32:40 +0530

    Update backend.tf

--- networking/backend.tf
+++ networking/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "anis-terraform-backend1"
+    bucket = "suraj-terraform-backend1"
     key    = "networking/terraform.tfstate"
     region = "ap-south-1"
   }
